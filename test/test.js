75a0ff6c77fa6bd3da286278b9fe05ef971cab42
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-14 16:35:48 -0500

    Added tests for GET /auth/google/callback

--- test/test.js
+++ test/test.js
@@ -7,12 +7,23 @@ chai.use(require('chai-http'));
 const app = require('../dist/index.js');
 
 describe('GET /auth/login', function () {
-    it('redirects to a google authentication page.', function () {
+    it('Returns status code 200 (Ok)', function () {
         chai.request(app)
             .get('/auth/login')
             .send()
             .then(function (res) {
                 expect(res).to.have.status(200);
+            }).catch(function (err) {
+                throw err;
+            }
+        );
+    });
+
+    it('Redirects to an authentication page on google.com', function () {
+        chai.request(app)
+            .get('/auth/login')
+            .send()
+            .then(function (res) {
                 expect(res).to.redirect;
                 expect(res).to.redirectTo(/google\.com/);
             }).catch(function (err) {
@@ -20,4 +31,31 @@ describe('GET /auth/login', function () {
             }
         );
     });
+});
+
+describe('GET /auth/google/callback', function () {
+    it('Returns status code 401 (Unauthorized)', function () {
+        chai.request(app)
+            .get('/auth/google/callback')
+            .send()
+            .then(function (res) {
+                expect(res).to.have.status(401);
+            }).catch(function (err) {
+                throw err;
+            }
+        );
+    });
+
+    it('Redirects to /auth/login', function () {
+        chai.request(app)
+            .get('/auth/google/callback')
+            .send()
+            .then(function (res) {
+                expect(res).to.redirect;
+                expect(res).to.redirectTo('/auth/login');
+            }).catch(function (err) {
+                throw err;
+            }
+        );
+    })
 });
