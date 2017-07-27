da609c9b5e969e3361f871bfb1706ce1c85aaa51
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-27 12:57:54 -0500

    Added a basic webpack config with loaders for babel and css, and common chunks for vendor bundling

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,42 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.js')
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        filename: 'client.bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'public', 'index.html')
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor.js',
+            minChunks: 2
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [ /node_modules/ ],
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [ 'es2015' ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    }
 };
