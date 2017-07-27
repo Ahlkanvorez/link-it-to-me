ea48c787b50c75c2b1596bbde181dcc970953fed
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-27 13:43:37 -0500

    Extracted the LoginStatusWarning component to its own file in the components directory.

--- client/src/app.js
+++ client/src/app.js
@@ -5,21 +5,7 @@ import MessageList from './messages.js';
 import MessageForm from './messageForm.js';
 import Navbar from './components/Navbar';
 import MessageLinkInfo from './components/MessageLinkInfo';
-
-const LoginStatusWarning = ({ username }) => (
-    <div>
-        { username === 'Anonymous'
-            ? (
-                <p>
-                    Warning: it seems you are sending messages anonymously. That's fine if it's what you want, but keep
-                    in mind that you cannot recover or view your messages if you lose them. If you login, you can view
-                    any messages (at least until they explode) that you make while logged in.
-                </p>
-            )
-            : null
-        }
-    </div>
-);
+import LoginStatusWarning from './components/LoginStatusWarning';
 
 class App extends React.Component {
     constructor (props) {
---
+++ client/src/components/LoginStatusWarning.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LoginStatusWarning = ({ username }) => (
+    <div>
+        { username === 'Anonymous'
+            ? (
+                <p>
+                    Warning: it seems you are sending messages anonymously. That's fine if it's what you want, but keep
+                    in mind that you cannot recover or view your messages if you lose them. If you login, you can view
+                    any messages (at least until they explode) that you make while logged in.
+                </p>
+            )
+            : null
+        }
+    </div>
+);
+
+export default LoginStatusWarning;
