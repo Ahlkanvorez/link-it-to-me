27310b570e0b98514bb34eec8ea77f7a54d0df30
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-27 13:52:45 -0500

    Extracted the RemovableListComponent to its own file in the components directory.

--- client/src/editableList.js
+++ client/src/editableList.js
@@ -1,29 +1,5 @@
 import React from 'react';
-
-class RemovableListInput extends React.Component {
-    constructor (props) {
-        super(props);
-
-        this.onInputChange = this.onInputChange.bind(this);
-    }
-
-    onInputChange (event) {
-        this.props.onChange(this.props.value, event.target.value);
-    }
-
-    render () {
-        return (
-            <tr>
-                <td>
-                    <input type="text" className="form-control" value={this.props.value} onChange={this.onInputChange} />
-                </td>
-                <td>
-                    <button className="btn btn-default" style={{ width: '100%' }} type="button" onClick={() => this.props.onDelete(this.props.value)}>Delete</button>
-                </td>
-            </tr>
-        );
-    }
-}
+import RemovableListInput from './components/RemovableListInput';
 
 class EditableList extends React.Component {
     constructor (props) {
