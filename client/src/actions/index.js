a0777bbc6909dbb1e109d443d7576bf7c1e6b9bd
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-27 20:39:25 -0500

    Improved the actions and reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,15 +4,16 @@ export const SET_EXPIRATION_DATE = 'SET_EXPIRATION_DATE';
 export const SET_MAXIMUM_ACCESSES = 'SET_MAXIMUM_ACCESSES';
 export const ADD_WHITELISTED_IP = 'ADD_WHITELISTED_IP';
 export const REMOVE_WHITELISTED_IP = 'REMOVE_WHITELISTED_IP';
+export const ADD_MESSAGE = 'ADD_MESSGE';
 export const SET_USERNAME = 'GET_USERNAME';
 
 // Action creators
-export function setMessageCONTENT (content) {
+export function setMessageContent (content) {
     return { type: SET_MESSAGE_CONTENT, content };
 };
 
-export function setExpirationDate (date) {
-    return { type: SET_EXPIRATION_DATE, date };
+export function setExpirationDate (expirationDate) {
+    return { type: SET_EXPIRATION_DATE, expirationDate };
 };
 
 export function setMaximumAccesses (maxAccesses) {
@@ -26,6 +27,10 @@ export function addWhitelistedIp (ip) {
 export function removeWhitelistedIp (ip) {
     return { type: REMOVE_WHITELISTED_IP, ip };
 };
+
+export function addMessage (message) {
+    return { type: ADD_MESSAGE, message };
+}
 
 export function setUsername (username) {
     return { type: SET_USERNAME, username };
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,65 +5,61 @@ import {
     SET_MAXIMUM_ACCESSES,
     ADD_WHITELISTED_IP,
     REMOVE_WHITELISTED_IP,
+    ADD_MESSAGE,
     SET_USERNAME
 } from '../actions/actions';
 
-function user (state, action) {
+function username (state = {}, action) {
     switch (action) {
         case SET_USERNAME:
-            return Object.assign({}, state, {
-                username: action.username
-            });
+            return action.username;
         default:
             return state;
     }
 }
 
-function messages (state = [], action) {
+function message (state = {}, action) {
     switch (action.type) {
         case SET_MESSAGE_CONTENT:
-            return [
-                ...state,
-                {
-                    content: action.content
-                }
-            ];
+            return Object.assign({}, state, {
+                content: action.content
+            });
         case SET_EXPIRATION_DATE:
-            return [
-                ...state,
-                {
-                    expires: action.date
-                }
-            ];
+            return Object.assign({}, state, {
+                expirationDate: action.expirationDate
+            });
         case SET_MAXIMUM_ACCESSES:
-            return [
-                ...state,
-                {
-                    maxAccesses: action.maxAccesses
-                }
-            ];
+            return Object.assign({}, state, {
+                maxAccesses: action.maxAccesses
+            });
         case ADD_WHITELISTED_IP:
-            return [
-                ...state,
-                {
-                    ipWhitelist: state.ipWhitelist.concat(action.ip)
-                }
-            ];
+            return Object.assign({}, state, {
+                ipWhitelist: state.ipWhitelist.concat(action.ip)
+            });
         case REMOVE_WHITELISTED_IP:
-            return [
-                ...state,
-                {
-                    ipWhitelist:
-                        state.ipWhitelist.filter(ip => ip !== action.ip)
-                }
-            ];
+            return Object.assign({}, state, {
+                ipWhitelist: state.ipWhitelist.filter(ip => ip !== action.ip)
+            });
         default:
             return state;
     }
 };
 
+function messages (state = [], action) {
+    switch (action.type) {
+        case ADD_MESSAGE:
+            return [
+                ...state,
+                action.message
+            ];
+        default:
+            return state;
+    }
+}
+
 const messageApp = combineReducers({
-    user,
+    username,
+    message,
     messages
 });
 
