966802b4c29bc29c55aec807f099d31f8a22776c
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-28 10:18:20 -0500

    Added prop-types to the MessageLinkInfo component, and simplified its implementation

--- client/src/components/MessageLinkInfo.js
+++ client/src/components/MessageLinkInfo.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const MessageLinkInfo = ({ id, messageContent }) => 
-    (id ? (
-        <div style={{
-                display: 'table',
-                margin: '0 auto',
-                paddingTop: '30px'
-            }}>
-            <p>
-                <a href={`/view/${id}`}>This link</a> will take you to your
-                secret message, but be careful! If you cause it to self-destruct
-                before your intended recipient can see it, you'll have to make
-                another!
-            </p>
-            <blockquote>
-                { messageContent }
-            </blockquote>
-        </div>
-    ) : null
+const MessageLinkInfo = ({ id, content }) => (
+    <div style={{
+            display: 'table',
+            margin: '0 auto',
+            paddingTop: '30px'
+        }}>
+        <p>
+            <a href={`/view/${id}`}>This link</a> will take you to your
+            secret message, but be careful! If you cause it to self-destruct
+            before your intended recipient can see it, {`you'll`} have to
+            make another!
+        </p>
+        <blockquote>
+            { content }
+        </blockquote>
+    </div>
 );
+
+MessageLinkInfo.propTypes = {
+    id: PropTypes.number.isRequired,
+    content: PropTypes.string.isRequired
+};
 
 export default MessageLinkInfo;
