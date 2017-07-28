fdeacc1b0f5024f3bc132cce4dd7fe2e6f4fefa8
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-28 10:22:59 -0500

    Added prop types to and removed the className parameter from the MessageList component.

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,13 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Message from './Message';
 
-const MessageList = ({ className, messages }) => (
+const MessageList = ({ messages }) => (
     (messages && messages.length > 0) ? (
-        <div className={ className }
-                style={{
-                    display: 'table',
-                    margin: '0 auto'
-                }}>
+        <div style={{ display: 'table', margin: '0 auto' }}>
             <h3>Your Messages</h3>
             <ul style={{ listStyleType: 'none' }}>
                 {
@@ -21,5 +18,22 @@ const MessageList = ({ className, messages }) => (
         </div>
     ) : null
 );
+
+MessageList.propTypes = {
+    messages: PropTypes.arrayOf(
+        PropTypes.objectOf(
+            PropTypes.shape({
+                content: PropTypes.string.isRequired,
+                expires: PropTypes.instanceOf(Date).isRequired,
+                maxAccesses: PropTypes.number.isRequired,
+                ipWhitelist: PropTypes.arrayOf(
+                    PropTypes.shape([
+                        PropTypes.string
+                    ]).isRequired
+                ).isRequired
+            }).isRequired
+        ).isRequired
+    ).isRequired
+};
 
 export default MessageList;
