6ab14a1d5136d7f7e4a1f2197fd4ee377fb72df7
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-28 10:17:14 -0500

    Added prop types to the LoginStatusWarning component, and wrapped the paragraph text in {} to satisfy eslint concerns about raw quotes.

--- client/src/components/LoginStatusWarning.js
+++ client/src/components/LoginStatusWarning.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const LoginStatusWarning = ({ username }) => (
     <div>
         { username === 'Anonymous'
             ? (
                 <p>
-                    Warning: it seems you are sending messages anonymously.
+                    {
+                    `Warning: it seems you are sending messages anonymously.
                     That's fine if it's what you want, but keep in mind that you
                     cannot recover or view your messages if you lose them. If
                     you login, you can view any messages (at least until they
-                    explode) that you make while logged in.
+                    explode) that you make while logged in.`
+                    }
                 </p>
             )
             : null
         }
     </div>
 );
+
+LoginStatusWarning.propTypes = {
+    username: PropTypes.string.isRequired
+};
 
 export default LoginStatusWarning;
