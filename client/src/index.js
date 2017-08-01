1bb9befb4dcb4a80a18cfdb7f497379625bf0975
Robert Mitchell <robert.mitchell36@gmail.com>
2017-08-01 17:42:39 -0500

    Added middleware for thunking and logging to store.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,22 @@
+import 'babel-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import MessageApp from './app';
 import registerServiceWorker from './registerServiceWorker';
 import messageApp from './reducers';
 
-const store = createStore(messageApp);
+const store = createStore(
+    messageApp,
+    applyMiddleware(
+        thunkMiddleware,
+        createLogger()
+    )
+);
 
 ReactDOM.render(
     <Provider store={store}>
